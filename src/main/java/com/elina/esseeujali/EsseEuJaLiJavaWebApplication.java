931bf7c0465b495cf327746ab838c545c63f783e
Elina Batista <elina.batista@capgemini.com>
2024-01-26 14:01:44 -0300

    Project esse-eu-ja-li-javaWeb created

---
+++ src/main/java/com/elina/esseeujali/EsseEuJaLiJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.elina.esseeujali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsseEuJaLiJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsseEuJaLiJavaWebApplication.class, args);
+	}
+
+}
